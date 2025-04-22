11490a25fe2448fc5233ca90d746d41bc9d6f48e
250 <61777586+hylee-250@users.noreply.github.com>
2025-04-22 18:39:06 +0900

    Create privacy.tsx

---
+++ pages/privacy.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+      <p>We do not store your data. All Make access is temporary and based on your consent.</p>
+    </>
+  );
+}
